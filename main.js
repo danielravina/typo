da44f0c3d775858a538f6b86dfd1a21a790a5807
Daniel Ravina <raabhim@gmail.com>
2019-12-27 16:31:19 -0800

    change class to pure function

--- main.js
+++ main.js
@@ -1,100 +1,46 @@
 const electron = require("electron");
-const {
-  clipboard,
-  ipcMain,
-  Menu,
-  app,
-  BrowserWindow,
-  Tray,
-  globalShortcut
-} = electron;
-
+const { clipboard, ipcMain, globalShortcut, shell } = electron;
+const { menubar } = require("menubar");
 const { version } = require("./package.json");
 const isDev = process.env.NODE_ENV === "development";
 const path = require("path");
 const robot = require("robotjs");
 const axios = require("axios");
-// const cheerio = require("cheerio");
-let tray;
-let win;
-let extraWin;
+
+const mb = menubar({
+  preloadWindow: true,
+  browserWindow: {
+    width: 321,
+    height: 235,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    },
+    alwaysOnTop: true
+  },
+  index: "http://127.0.0.1:9000"
+});
 
 function hideWindow() {
-  win.hide();
-  app.hide();
-  win.setAlwaysOnTop(false);
-  win.webContents.send("window-hidden");
-  win.setVisibleOnAllWorkspaces(false);
-  win.setFullScreenable(true);
+  mb.window.hide();
+  mb.app.hide();
+  mb.window.webContents.send("window-hidden");
 }
 
 function showWindow() {
-  win.show();
-  app.show();
-  win.webContents.send("window-shown");
-  win.setAlwaysOnTop(true, "floating");
-  win.setVisibleOnAllWorkspaces(true);
-  win.setFullScreenable(false);
+  mb.showWindow();
+  mb.window.webContents.send("window-shown");
 }
 
 function toggleWindow() {
-  if (win.isVisible()) {
+  if (mb.window.isVisible()) {
     hideWindow();
   } else {
     showWindow();
   }
 }
 
-function initTray() {
-  tray = new Tray(path.join(__dirname, "cloudTemplate.png"));
-  tray.setToolTip("typo");
-  tray.setContextMenu(
-    Menu.buildFromTemplate([
-      {
-        label: "Toggle",
-        accelerator: "Command+Control+Space",
-        click: toggleWindow
-      },
-      { label: `typo v${version}`, enabled: false },
-      { type: "separator" },
-      { label: `Quit`, click: () => app.quit() }
-    ])
-  );
-}
-
-function createWindow() {
-  win = new BrowserWindow({
-    width: 520,
-    height: 185,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js")
-    },
-    resizable: true,
-    title: "typo",
-    maximizable: false,
-    transparent: true,
-    frame: false,
-    show: isDev,
-    center: true
-  });
-
-  if (isDev) {
-    win.loadURL("http://127.0.0.1:9000");
-  } else {
-    win.loadFile("./build/index.html");
-  }
-
-  win.on("closed", () => {
-    win = null;
-  });
-
-  win.on("blur", () => {
-    if (!isDev) hideWindow();
-  });
-
-  ipcMain.on("hide", () => {
-    hideWindow();
-  });
+mb.on("ready", () => {
+  globalShortcut.register("Control+Space", toggleWindow);
 
   ipcMain.on("copyClipBoard", (_, value) => {
     clipboard.writeText(value);
@@ -104,56 +50,24 @@ function createWindow() {
   });
 
   ipcMain.on("openExternal", async (_, { value, source }) => {
+    if (source === "google.com") {
+      shell.openExternal(
+        `https://www.google.com/search?q=${value}&sourceid=chrome&ie=UTF-8`
+      );
+      return;
+    }
+
     const { data } = await axios(
-      `https://www.google.ca/search?q=site%3A${source}+${value}&sourceid=chrome&ie=UTF-8`
+      `https://www.google.com/search?q=site%3A${source}+${value}&sourceid=chrome&ie=UTF-8`
     );
 
     const match = data.match(/url\?q=([a-zA-z:\/\/\.-]*)/);
     const link = match[1];
 
-    if (!extraWin) {
-      extraWin = new BrowserWindow({
-        parent: win,
-        width: 520,
-        height: 520,
-        resizable: false,
-        maximizable: false,
-        minimizable: false,
-        movable: false,
-        title: "Loading..."
-        transparent: true,
-        frame: false
-      });
-      extraWin.on("close", e => {
-        e.preventDefault();
-        extraWin.hide();
-        extraWin.loadFile("./");
-      });
-    } else {
-      extraWin.loadURL("");
-    }
-
-    const [x, y] = win.getPosition();
-    const [width, height] = win.getSize();
-
-    extraWin.show();
-    extraWin.loadURL(link);
-    extraWin.setPosition(x, y + height, false);
-    extraWin.on();
+    shell.openExternal(link);
   });
-}
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
-
-app.on("activate", () => {
-  if (win === null) createWindow();
-});
-
-app.on("ready", () => {
-  app.dock.hide();
-  createWindow();
-  initTray();
-  globalShortcut.register("Control+Space", toggleWindow);
+  ipcMain.on("hide", () => {
+    hideWindow();
+  });
 });
--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.scss";
 
-import React from "react";
+import React, {
+  useMemo,
+  useRef,
+  useState,
+  useCallback,
+  useEffect
+} from "react";
 import emoji from "node-emoji";
 import classnames from "classnames";
 import fetchJsonp from "fetch-jsonp";
@@ -14,340 +20,432 @@ import logoBright from "./images/logo-bright.svg";
 const SUGGESTIONS_URL =
   "https://suggestqueries.google.com/complete/search?client=firefox&q=";
 
-const defaultEmojis = ["👍", "😂", "🔥", "🙂", "🎉", "😭", "😍", "💥", "🤔"];
+const defaultEmojis = [
+  "👍",
+  "😂",
+  "🔥",
+  "😊",
+  "🎉",
+  "😭",
+  "😍",
+  "💥",
+  "🤔",
+  "💕",
+  "🙏",
+  "🤣",
+  "❤️",
+  "👎",
+  "👌"
+];
 
-const defaultState = {
-  word: "",
-  suggestion: "",
-  isTitleized: false,
-  emojis: defaultEmojis,
-  selectedIndex: 0,
-  mode: "lazy",
-  wordSelection: false,
-  selectionCount: defaultEmojis.length,
-  colorTheme: "dark",
-  suggestionHistory: []
+const altOptions = {
+  google: {
+    key: "G",
+    address: "google.com",
+    label: "oogle",
+    icon: require("./images/google_logo.png")
+  },
+  wikipedia: {
+    key: "W",
+    address: "wikipedia.org",
+    label: "ikipedia",
+    icon: require("./images/wiki_logo.png")
+  },
+  thesaurus: {
+    key: "T",
+    address: "thesaurus.com",
+    label: "hesaurus",
+    icon: require("./images/theasaurus_logo.png")
+  },
+  dictionary: {
+    key: "D",
+    address: "dictionary.com",
+    label: "ictionary",
+    icon: require("./images/dict_logo.png")
+  }
 };
 
-class App extends React.Component {
-  state = defaultState;
+export default function() {
+  const [word, setWord] = useState("");
+  const [suggestion, setSuggestion] = useState("");
+  const [altPressed, setAltPressed] = useState(false);
+  const [shiftPressed, setShiftPressed] = useState(false);
+  const [emojiMode, setEmojiMode] = useState(false);
+  const [emojis, setEmojis] = useState(defaultEmojis);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [wordSelectionMode, setWordSelectionMode] = useState(false);
+  const [selectionCount, setSelectionCount] = useState(defaultEmojis.length);
+  const [colorTheme, setColorTheme] = useState(null);
+  const [suggestionHistory, setSuggestionHistory] = useState([]);
 
-  componentDidMount() {
-    window.ipcRenderer.on("window-shown", () => {
-      this.input.focus();
-    });
-  }
+  const resetState = useCallback(() => {
+    setWord("");
+    setSuggestion("");
+    setAltPressed(false);
+    setShiftPressed(false);
+    setEmojiMode(false);
+    setEmojis(defaultEmojis);
+    setSelectedIndex(0);
+    setWordSelectionMode(false);
+    setSelectionCount(defaultEmojis.length);
+  }, []);
 
-  onInputChange = e => {
-    const { value } = e.target;
-    this.setState({ word: value });
-    if (value.charAt(0) === ":") {
-      this.setState({ mode: "emoji", emojis: defaultEmojis, selectedIndex: 0 });
-      this.fetchEmojis(value);
-    } else {
-      this.setState({
-        mode: "lazy",
-        emojis: defaultEmojis,
-        wordSelection: false,
-        selectedIndex: 0,
-        selectionCount: defaultEmojis.length
-      });
-      this.fetchSuggestions(value);
-    }
-  };
+  const input = useRef(null);
 
-  onKeyUp = e => {
-    switch (e.key) {
-      case "Shift":
-        this.setState({ isTitleized: false });
-        break;
-      default:
-        break;
-    }
-  };
-
-  onKeyDown = e => {
-    switch (e.key) {
-      case "Shift":
-        this.setState({ isTitleized: true });
-        break;
-      case "Escape":
-        window.ipcRenderer.send("hide");
-        this.setState(defaultState);
-        break;
-      case "Tab":
-        this.onTabPress();
-        break;
-      case "Enter":
-        this.onEnterPress();
-        break;
-      case "ArrowRight":
-        if (this.state.wordSelection) e.preventDefault();
-        this.onArrowRight();
-        break;
-      case "ArrowLeft":
-        if (this.state.wordSelection) e.preventDefault();
-        this.onArrowLeft();
-        break;
-      case "ArrowUp":
-        e.preventDefault();
-        this.onArrowUp();
-        break;
-      case "ArrowDown":
-        e.preventDefault();
-        this.onArrowDown();
-        break;
-      default:
-        break;
-    }
-  };
-
-  onArrowUp = () => {
-    this.setState({ wordSelection: true });
-  };
-
-  onArrowDown = () => {
-    this.setState({ wordSelection: false, selectedIndex: 0 });
-  };
-
-  onArrowLeft = () => {
-    const { selectionCount, selectedIndex } = this.state;
-
-    if (selectedIndex > 0) {
-      this.setState({ selectedIndex: selectedIndex - 1 });
-    } else {
-      this.setState({ selectedIndex: selectionCount - 1 });
-    }
-  };
-
-  onArrowRight = () => {
-    const { selectionCount, selectedIndex } = this.state;
-
-    if (selectedIndex < selectionCount - 1) {
-      this.setState({ selectedIndex: selectedIndex + 1 });
-    } else {
-      this.setState({ selectedIndex: 0 });
-    }
-  };
-
-  get selectedWord() {
-    const { wordSelection, mode, emojis, selectedIndex } = this.state;
-
-    if (mode === "emoji") {
-      return emojis[selectedIndex];
-    } else if (wordSelection === true) {
-      return this.suggestion.split(" ")[selectedIndex];
-    } else {
-      return this.suggestion;
-    }
-  }
-
-  onEnterPress = () => {
-    const { suggestionHistory } = this.state;
-
-    let target = this.selectedWord;
-
-    window.ipcRenderer.send("copyClipBoard", target);
-    window.ipcRenderer.send("hide");
-    const newHistory = Array.from(
-      new Set([...suggestionHistory, target.toLowerCase()])
-    );
-    this.setState({
-      ...defaultState,
-      suggestionHistory: newHistory
-    });
-  };
-
-  onTabPress() {
-    setTimeout(() => {
-      this.input.focus();
-      var val = this.state.suggestion + " ";
-      this.input.value = "";
-      this.input.value = val;
-      this.setState({ word: val, wordSelection: false });
-    }, 5);
-  }
-
-  fetchEmojis(value) {
+  const fetchEmojis = useCallback(value => {
     if (value.length < 2) {
       // include the ':' which is stripped by node-emoji
-      this.setState({
-        selectedIndex: 0,
-        selectionCount: defaultEmojis.length
-      });
+      setSelectedIndex(0);
+      setSelectionCount(defaultEmojis.length);
+
       return;
     }
 
     const results = emoji
       .search(value)
       .map(e => e.emoji)
-      .slice(0, 9);
+      .slice(0, 15);
 
-    this.setState({
-      emojis: results,
-      selectedIndex: 0,
-      selectionCount: results.length
-    });
-  }
+    setEmojis(results);
+    setSelectedIndex(0);
+    setSelectionCount(results.length);
+  }, []);
 
-  async fetchSuggestions(value) {
-    if (!value.length) {
-      this.setState({ suggestion: "" });
-      return;
-    }
+  const fetchSuggestions = useCallback(
+    async value => {
+      if (!value.length) {
+        setSuggestion("");
+        return;
+      }
 
-    let suggestion;
+      let newSuggestion;
 
-    const history = fuzzy
-      .filter(value, this.state.suggestionHistory)
-      .map(s => s.original);
+      const history = fuzzy
+        .filter(value, suggestionHistory)
+        .map(s => s.original);
 
-    if (history.length) {
-      suggestion = history[0];
+      if (history.length) {
+        newSuggestion = history[0];
+      } else {
+        const response = await fetchJsonp(SUGGESTIONS_URL + value);
+        const results = await response.json();
+
+        newSuggestion = results[1][0] || "";
+      }
+
+      setSuggestion(newSuggestion);
+      setSelectionCount(newSuggestion.split(" ").length);
+    },
+    [suggestionHistory]
+  );
+
+  const formattedSuggestion = useMemo(() => {
+    if (shiftPressed) {
+      return titleCase(suggestion);
     } else {
-      const response = await fetchJsonp(SUGGESTIONS_URL + value);
-      const results = await response.json();
-      suggestion = results[1][0] || "";
+      return suggestion;
     }
+  }, [suggestion, shiftPressed]);
 
-    this.setState({
-      suggestion,
-      emojis: defaultEmojis,
-      selectionCount: suggestion.split(" ").length
-    });
-  }
-
-  get suggestion() {
-    if (this.state.isTitleized) {
-      return titleCase(this.state.suggestion);
+  const selectedWord = useMemo(() => {
+    if (emojiMode) {
+      return emojis[selectedIndex];
+    } else if (wordSelectionMode === true) {
+      return formattedSuggestion.split(" ")[selectedIndex];
     } else {
-      return this.state.suggestion;
+      return formattedSuggestion;
     }
-  }
+  }, [
+    emojiMode,
+    emojis,
+    formattedSuggestion,
+    selectedIndex,
+    wordSelectionMode
+  ]);
 
-  shouldHighlight() {
-    return !this.state.wordSelection;
-  }
+  useEffect(() => {
+    window.ipcRenderer.on("window-shown", () => {
+      input.current.focus();
+    });
+    window.ipcRenderer.on("window-hidden", () => {
+      resetState();
+    });
+  }, [resetState]);
 
-  isWordMatchSuggestion() {
-    return (
-      this.suggestion.trim().toLowerCase() ===
-        this.state.word.trim().toLowerCase() || this.state.isTitleized //
-    );
-  }
+  const onExternalSelect = useCallback(
+    source => {
+      window.ipcRenderer.send("hide");
+      window.ipcRenderer.send("openExternal", {
+        value: selectedWord,
+        source
+      });
 
-  renderSuggestion() {
-    const twoLines = this.suggestion.length > 20;
-
-    if (this.isWordMatchSuggestion() && !this.state.wordSelection) {
-      return (
-        <span
-          className={classnames("suggestion-text animated fadeIn selected", {
-            reduced: twoLines
-          })}
-        >
-          {this.suggestion}
-        </span>
+      const newHistory = Array.from(
+        new Set([...suggestionHistory, selectedWord.toLowerCase()])
       );
+
+      setSuggestionHistory(newHistory);
+    },
+    [selectedWord, suggestionHistory]
+  );
+
+  const onTabPress = useCallback(() => {
+    input.current.focus();
+    const val = suggestion + " ";
+    input.current.value = "";
+    input.current.value = val;
+    setWord(val);
+    setWordSelectionMode(false);
+  }, [suggestion]);
+
+  const onInputChange = useCallback(
+    e => {
+      const { value } = e.target;
+      setWord(value);
+
+      if (value.charAt(0) === ":") {
+        setEmojiMode(true);
+        setEmojis(defaultEmojis);
+        setSelectedIndex(0);
+
+        fetchEmojis(value);
+      } else {
+        setEmojiMode(false);
+        setEmojis(defaultEmojis);
+        setSelectedIndex(0);
+        setSelectionCount(defaultEmojis.length);
+
+        fetchSuggestions(value);
+      }
+    },
+    [fetchEmojis, fetchSuggestions]
+  );
+
+  const onKeyUp = useCallback(e => {
+    switch (e.key) {
+      case "Shift":
+        setShiftPressed(false);
+        break;
+      case "Alt":
+        setAltPressed(false);
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+  const onKeyPress = useCallback(
+    e => {
+      if (altPressed) {
+        e.preventDefault();
+        switch (e.key) {
+          case "∑":
+            onExternalSelect("wikipedia.org");
+            break;
+          case "∂":
+            onExternalSelect("dictionary.com");
+            break;
+          case "†":
+            onExternalSelect("thesaurus.com");
+            break;
+          case "©":
+            onExternalSelect("google.com");
+            break;
+          default:
+            break;
+        }
+      }
+    },
+    [altPressed, onExternalSelect]
+  );
+
+  const onEnterPress = useCallback(() => {
+    window.ipcRenderer.send("copyClipBoard", selectedWord);
+    window.ipcRenderer.send("hide");
+
+    const newHistory = Array.from(
+      new Set([...suggestionHistory, selectedWord.toLowerCase()])
+    );
+
+    setSuggestionHistory(newHistory);
+  }, [selectedWord, suggestionHistory]);
+
+  const onArrowUp = useCallback(() => {
+    setWordSelectionMode(false);
+    setSelectedIndex(0);
+  }, []);
+
+  const onArrowDown = useCallback(() => {
+    setWordSelectionMode(true);
+  }, []);
+
+  const onArrowLeft = useCallback(() => {
+    if (selectedIndex > 0) {
+      setSelectedIndex(selectedIndex - 1);
+    } else {
+      setSelectedIndex(selectionCount - 1);
+    }
+  }, [selectedIndex, selectionCount]);
+
+  const onArrowRight = useCallback(() => {
+    if (!wordSelectionMode) {
+      setWordSelectionMode(true);
     }
 
-    return (
-      <span
-        className={classnames("suggestion-text animated fadeIn", {
-          reduced: twoLines
+    if (selectedIndex < selectionCount - 1) {
+      setSelectedIndex(selectedIndex + 1);
+    } else {
+      setSelectedIndex(0);
+    }
+  }, [selectedIndex, selectionCount, wordSelectionMode]);
+
+  const onKeyDown = useCallback(
+    e => {
+      switch (e.key) {
+        case "Shift":
+          setShiftPressed(true);
+          break;
+        case "Alt":
+          setAltPressed(true);
+          break;
+        case "Escape":
+          window.ipcRenderer.send("hide");
+          break;
+        case "Tab":
+          e.preventDefault();
+          onTabPress();
+          break;
+        case "Enter":
+          onEnterPress();
+          break;
+        case "ArrowRight":
+          if (wordSelectionMode) e.preventDefault();
+          onArrowRight();
+          break;
+        case "ArrowLeft":
+          if (wordSelectionMode) e.preventDefault();
+          onArrowLeft();
+          break;
+        case "ArrowUp":
+          e.preventDefault();
+          onArrowUp();
+          break;
+        case "ArrowDown":
+          e.preventDefault();
+          onArrowDown();
+          break;
+        default:
+          break;
+      }
+    },
+    [
+      onArrowDown,
+      onArrowLeft,
+      onArrowRight,
+      onArrowUp,
+      onEnterPress,
+      onTabPress,
+      wordSelectionMode
+    ]
+  );
+
+  // renderEmojis() {
+  //   if (!emojiMode) return null;
+
+  //   return (
+  //     <div className="emoji-wrapper">
+  //       {emojis.map((e, i) => (
+  //         <span
+  //           key={i}
+  //           className={classnames("animated", "bounceIn", "emoji", {
+  //             selected: i === selectedIndex
+  //           })}
+  //           dangerouslySetInnerHTML={{ __html: e }}
+  //         />
+  //       ))}
+  //     </div>
+  //   );
+  // }
+
+  const twoLines = useMemo(() => suggestion.length > 30, [suggestion]);
+
+  const isWordMatchSuggestion = useMemo(
+    () =>
+      word.length &&
+      suggestion.length &&
+      suggestion.trim().toLowerCase() === word.trim().toLowerCase(),
+
+    [suggestion, word]
+  );
+
+  const suggestionParts = useMemo(
+    () =>
+      formattedSuggestion.split(" ").map((w, i) => (
+        <>
+          <span
+            className={classnames("word", {
+              selected: word.length && wordSelectionMode && i === selectedIndex
+            })}
+          >
+            <Highlighter
+              highlightClassName={"selected"}
+              searchWords={
+                wordSelectionMode ? [] : word.split("").map(RegexEscape)
+              }
+              textToHighlight={w}
+            />
+          </span>
+          <span className="spacer" />
+        </>
+      )),
+    [formattedSuggestion, selectedIndex, word, wordSelectionMode]
+  );
+
+  return (
+    <div className={`app ${colorTheme}`}>
+      <div className="input-wrapper">
+        <input
+          autoFocus
+          ref={input}
+          className="main-input"
+          value={word}
+          onChange={onInputChange}
+          onKeyDown={onKeyDown}
+          onKeyUp={onKeyUp}
+          onKeyPress={onKeyPress}
+        />
+      </div>
+      <div className="suggestion-wrapper">
+        <div className="suggestion-body">
+          <span
+            className={classnames("suggestion-text animated fadeIn", {
+              reduced: twoLines,
+              selected: !wordSelectionMode && isWordMatchSuggestion
+            })}
+          >
+            {suggestionParts}
+          </span>
+        </div>
+      </div>
+      <div
+        className={classnames("alt-options", {
+          active: altPressed && word.length
         })}
       >
-        {this.suggestion.split(" ").map((w, i) => (
-          <>
-            <span
-              className={classnames("word", {
-                selected:
-                  this.state.wordSelection && i === this.state.selectedIndex
-              })}
-            >
-              <Highlighter
-                highlightClassName={"selected"}
-                searchWords={
-                  this.shouldHighlight()
-                    ? this.state.word.split("").map(RegexEscape)
-                    : []
-                }
-                textToHighlight={w}
-              />
-            </span>
-            <span className="spacer" />
-          </>
-        ))}
-      </span>
-    );
-  }
-
-  renderEmojis() {
-    if (this.state.mode !== "emoji") return null;
-    return (
-      <div className="emoji-wrapper">
-        {this.state.emojis.map((e, i) => (
-          <span
-            key={i}
-            className={classnames("animated", "bounceIn", "emoji", {
-              selected: i === this.state.selectedIndex
-            })}
-            dangerouslySetInnerHTML={{ __html: e }}
-          />
-        ))}
-      </div>
-    );
-  }
-
-  render;
-
-  render() {
-    return (
-      <div className={`app ${this.state.colorTheme}`}>
-        <header>
-          <button
-            onClick={() => {
-              window.ipcRenderer.send("openExternal", {
-                value: this.selectedWord,
-                source: "wikipedia.org"
-                // source: "thesaurus.com"
-                // source: "dictionary.com"
-                // source: "stackoverflow.com"
-                // source: "youtube.com"
-              });
-            }}
-          >
-            Wikipedia
-          </button>
-        </header>
-        <div className="suggestion-wrapper">
-          {this.suggestion || this.state.mode === "emoji" ? (
-            <div className="suggestion-body">
-              {this.renderEmojis() || this.renderSuggestion()}
+        {Object.keys(altOptions).map(alt => {
+          const { icon, key, label, address } = altOptions[alt];
+          return (
+            <div className="alt-option" key={alt}>
+              <div className="option-icon">
+                <img src={icon} alt={address} />
+              </div>
+              <span className="alt-option-key">{key}</span>
+              {label}
+              <i className="material-icons open-in-new">open_in_new</i>
             </div>
-          ) : (
-            <div className="logo-wrapper animated fadeIn faster">
-              <img src={this.state.colorTheme === "dark" ? logoBright : logo} />
-              <small>Start typing to see results...</small>
-            </div>
-          )}
-        </div>
-        <div className="input-wrapper">
-          <input
-            autoFocus
-            ref={r => (this.input = r)}
-            className="main-input"
-            disabled={this.state.locked}
-            value={this.state.word}
-            onChange={this.onInputChange}
-            onKeyDown={this.onKeyDown}
-            placeholder="Type Whatever"
-            onKeyUp={this.onKeyUp}
-          />
-        </div>
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
